f43c322553da9643732abd1a8a05101364e7ac31
Aleksandr Golodin <golodinaleksandr@gmail.com>
2024-03-06 19:33:57 +0100

    Добавление Gulp в проект

---
+++ client/gulpfile.js
@@ -0,0 +1,67 @@
+const gulp = require("gulp");
+const concat = require("gulp-concat-css");
+const plumber = require("gulp-plumber");
+const del = require("del");
+const browserSync = require("browser-sync").create();
+
+function serve() {
+  browserSync.init({
+    server: {
+      baseDir: "./dist",
+    },
+  });
+}
+
+function html() {
+  return gulp
+    .src("src/views/**/*.html")
+    .pipe(plumber())
+    .pipe(gulp.dest("dist/"))
+    .pipe(browserSync.reload({ stream: true }));
+}
+
+function css() {
+  return gulp
+    .src("src/styles/**/*.css")
+    .pipe(plumber())
+    .pipe(concat("bundle.css"))
+    .pipe(gulp.dest("dist/"))
+    .pipe(browserSync.reload({ stream: true }));
+}
+
+function images() {
+  return gulp
+    .src("src/images/**/*{jpg,png,gif,ico,webp,avif}")
+    .pipe(gulp.dest("dist/images"))
+    .pipe(browserSync.reload({ stream: true }));
+}
+
+function svg() {
+  return gulp
+    .src("src/svg/**/*.svg")
+    .pipe(gulp.dest("dist/svg"))
+    .pipe(browserSync.reload({ stream: true }));
+}
+
+function watchFiles() {
+  gulp.watch(["src/views/**/*.html"], html);
+  gulp.watch(["src/styles/**/*.css"], css);
+  gulp.watch(["src/images/**/*{jpg,png,gif,ico,webp,avif}"], images);
+  gulp.watch(["src/svg/**/*.svg"], svg);
+}
+
+function clean() {
+  return del("dist");
+}
+
+const build = gulp.series(clean, gulp.parallel(html, css, images, svg));
+const watchApp = gulp.parallel(build, watchFiles, serve);
+
+exports.build = build;
+exports.watchApp = watchApp;
+
+// exports.html = html;
+// exports.css = css;
+// exports.images = images;
+// exports.svg = svg;
+// exports.clean = clean;
