c6d098ade1083aec890045df841e04ebca410bff
Aleksandr Golodin <golodinaleksandr@gmail.com>
2024-03-07 13:46:39 +0100

    Рефакторинг кода

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -1,4 +1,5 @@
 const gulp = require("gulp");
+const gulpPug = require("gulp-pug");
 const concat = require("gulp-concat-css");
 const plumber = require("gulp-plumber");
 const del = require("del");
@@ -12,19 +13,20 @@ function serve() {
   });
 }
 
-function html() {
+function pug() {
   return gulp
-    .src("src/views/**/*.html")
+    .src("src/pages/**/*.pug")
     .pipe(plumber())
+    .pipe(gulpPug())
     .pipe(gulp.dest("dist/"))
     .pipe(browserSync.reload({ stream: true }));
 }
 
-function pug() {
+function html() {
   return gulp
-    .src("src/components/**/*.pug")
+    .src("src/pages/**/*.html")
     .pipe(plumber())
-    .pipe(gulp.dest("dist/components"))
+    .pipe(gulp.dest("dist/"))
     .pipe(browserSync.reload({ stream: true }));
 }
 
@@ -53,6 +55,7 @@ function svg() {
 
 function watchFiles() {
   gulp.watch(["src/views/**/*.html"], html);
+  gulp.watch(["src/pages/**/*.pug"], pug);
   gulp.watch(["src/styles/**/*.css"], css);
   gulp.watch(["src/images/**/*{jpg,png,gif,ico,webp,avif}"], images);
   gulp.watch(["src/svg/**/*.svg"], svg);
@@ -65,5 +68,10 @@ function clean() {
 const build = gulp.series(clean, gulp.parallel(html, pug, css, images, svg));
 const watchApp = gulp.parallel(build, watchFiles, serve);
 
+exports.html = html;
+exports.pug = pug;
+exports.css = css;
+exports.images = images;
+exports.svg = svg;
 exports.build = build;
 exports.watchApp = watchApp;
