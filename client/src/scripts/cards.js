cb0853b57c113e2437fe94eb887debd83fcde6a6
Aleksandr Golodin <golodinaleksandr@gmail.com>
2024-04-05 16:36:53 +0200

    Оптимизация кода

---
+++ client/src/scripts/cards.js
@@ -0,0 +1,103 @@
+const furCoats = [
+  {
+    name: "Шуба",
+    image: "../images/Rectangle-7.png",
+    description: "Описание шубы",
+  },
+  {
+    name: "Шуба",
+    image: "../images/Rectangle-7.png",
+    description: "Описание шубы",
+  },
+  {
+    name: "Шуба",
+    image: "../images/Rectangle-7.png",
+    description: "Описание шубы",
+  },
+  {
+    name: "Шуба",
+    image: "../images/Rectangle-7.png",
+    description: "Описание шубы",
+  },
+  {
+    name: "Шуба",
+    image: "../images/Rectangle-7.png",
+    description: "Описание шубы",
+  },
+  {
+    name: "Шуба",
+    image: "../images/Rectangle-7.png",
+    description: "Описание шубы",
+  },
+  {
+    name: "Шуба",
+    image: "../images/Rectangle-7.png",
+    description: "Описание шубы",
+  },
+];
+
+const coats = [
+  {
+    name: "Пальто",
+    image: "../images/Rectangle-10.png",
+    description: "Описание пальто",
+  },
+  {
+    name: "Пальто",
+    image: "../images/Rectangle-10.png",
+    description: "Описание пальто",
+  },
+  {
+    name: "Пальто",
+    image: "../images/Rectangle-10.png",
+    description: "Описание пальто",
+  },
+  {
+    name: "Пальто",
+    image: "../images/Rectangle-10.png",
+    description: "Описание пальто",
+  },
+  {
+    name: "Пальто",
+    image: "../images/Rectangle-10.png",
+    description: "Описание пальто",
+  },
+  {
+    name: "Пальто",
+    image: "../images/Rectangle-10.png",
+    description: "Описание пальто",
+  },
+];
+
+const parks = [
+  {
+    name: "Парка",
+    image: "../images/Rectangle-11.png",
+    description: "Описание парки",
+  },
+  {
+    name: "Парка",
+    image: "../images/Rectangle-11.png",
+    description: "Описание парки",
+  },
+  {
+    name: "Парка",
+    image: "../images/Rectangle-11.png",
+    description: "Описание парки",
+  },
+  {
+    name: "Парка",
+    image: "../images/Rectangle-11.png",
+    description: "Описание парки",
+  },
+  {
+    name: "Парка",
+    image: "../images/Rectangle-11.png",
+    description: "Описание парки",
+  },
+  {
+    name: "Парка",
+    image: "../images/Rectangle-11.png",
+    description: "Описание парки",
+  },
+];
