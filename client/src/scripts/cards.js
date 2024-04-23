3000502a038940ff11e2b0ff221699e1a7561ecd
Aleksandr Golodin <golodinaleksandr@gmail.com>
2024-04-23 19:37:42 +0200

    Настройка стилей для страницы basket

--- client/src/scripts/cards.js
+++ client/src/scripts/cards.js
@@ -2,39 +2,53 @@
 
 const furCoats = [
   {
+    id: 1,
     name: "Шуба",
     image: "../images/Rectangle-7.png",
     description: "Описание шубы",
+    price: 1000,
   },
   {
+    id: 2,
     name: "Шуба",
     image: "../images/Rectangle-7.png",
     description: "Описание шубы",
+    price: 1000,
   },
   {
+    id: 3,
     name: "Шуба",
     image: "../images/Rectangle-7.png",
     description: "Описание шубы",
+    price: 1000,
   },
   {
+    id: 4,
     name: "Шуба",
     image: "../images/Rectangle-7.png",
     description: "Описание шубы",
+    price: 1000,
   },
   {
+    id: 5,
     name: "Шуба",
     image: "../images/Rectangle-7.png",
     description: "Описание шубы",
+    price: 1000,
   },
   {
+    id: 6,
     name: "Шуба",
     image: "../images/Rectangle-7.png",
     description: "Описание шубы",
+    price: 1000,
   },
   {
+    id: 7,
     name: "Шуба",
     image: "../images/Rectangle-7.png",
     description: "Описание шубы",
+    price: 1000,
   },
 ];
 
