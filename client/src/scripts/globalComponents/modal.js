d321e74ecd07366c4346c68e4ae47173d9a2d796
Aleksandr Golodin <golodinaleksandr@gmail.com>
2024-05-13 22:25:26 +0200

    Добавлен скрипт закртия\открытия модальных окон

--- client/src/scripts/globalComponents/modal.js
+++ client/src/scripts/globalComponents/modal.js
@@ -1,9 +1,26 @@
+// @todo: Функция закрития модальных окон через кнопку esc
+const closeEscPopup = (evt) => {
+  if (evt.key === "Escape") {
+    const openedPopup = document.querySelector(".popup_opened");
+    if (openedPopup) {
+      openedPopup.classList.toggle("popup_opened");
+    }
+  }
+};
+
 // @todo: Функция открытия модального окна
-const openPopup = (modal) => {
-  modal.classList.add("popup_open");
+export const openPopup = (modal) => {
+  modal.classList.add("popup_opened");
+  closeEscPopup(modal);
+  document.addEventListener("keydown", closeEscPopup);
 };
 
 // @todo: Функция закрытия модального окна
-const closePopup = (modal) => {
-  modal.classList.remove("popup_open");
+export const closePopup = (modal) => {
+  if (modal.classList.contains("popup_opened")) {
+    modal.classList.remove("popup_opened");
+  }
+  document.removeEventListener("keydown", closeEscPopup);
 };
+
+// @todo: Функция закрития модальных окон через кнопку esc
